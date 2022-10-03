b5aca291a836abcd4aa98f59d3be355268b8c48c
Khushi Sharma <87928896+Khushi21S@users.noreply.github.com>
2022-10-03 13:39:21 +0530

    Sort an array of 0s, 1s and 2s

---
+++ Solutions/Sortanarrayof(0s,1s,2s).cpp
@@ -0,0 +1,14 @@
+//Problem link: https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
+
+//Solution
+
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        // code here 
+        sort(a,a+n);
+    }
+    
+};
