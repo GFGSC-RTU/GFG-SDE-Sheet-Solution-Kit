98813503defaad58bddccbc5276c22de56d1ad76
nipunagarwal9636 <58950264+nipunagarwal9636@users.noreply.github.com>
2022-10-05 00:21:47 +0530

    Update Reversearrayingroups.py

--- Solutions/Reversearrayingroups.py
+++ Solutions/Reversearrayingroups.py
@@ -2,9 +2,9 @@
 
 class Solution:	
     #Function to reverse every sub-array group of size k.
-	def reverseInGroups(self, arr, N, K):
-		# code here 
-		i=0
+    def reverseInGroups(self, arr, N, K):
+	# code here 
+	i=0
         while(i<=N):
             lst = arr[i:i+K][::-1]
             arr[i:i+K] = lst
