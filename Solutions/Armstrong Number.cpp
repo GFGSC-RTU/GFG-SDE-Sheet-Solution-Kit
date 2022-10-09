08c077dcb8b983f5bad18955a13d5b1a9f231e15
Amit Maurya <amitMaurya9669@gmail.com>
2022-10-09 13:56:24 +0530

    Added Armstrong Number Problem

---
+++ Solutions/Armstrong Number.cpp
@@ -0,0 +1,38 @@
+// Problem Link : https://leetcode.com/problems/armstrong-number/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool ArmstrongNumber(int n)
+{
+    int originalno = n;
+    int count = 0;
+    int temp = n;
+    while (temp != 0)
+    {
+        count++;
+        temp = temp / 10;
+    }
+    int sumofpower = 0;
+    while (n != 0)
+    {
+        int digit = n % 10;
+        sumofpower += pow(digit,count);
+        n /= 10;
+    }
+    return (sumofpower == originalno);
+}
+int main()
+{
+    int n1 = 153;
+    if (ArmstrongNumber(n1))
+    {
+        cout << "Yes, it is an Armstrong Number\n";
+    }
+    else
+    {
+        cout << "No, it is not an Armstrong Number\n";
+    }
+    return 0;
+}
