fa76acb4aa6434fcd2b4118904e93a1ea87526be
Srishti Manna <85846340+srishti666@users.noreply.github.com>
2022-10-09 18:56:41 +0530

    Create reverse words.py

---
+++ Solutions/reverse words.py
@@ -0,0 +1,16 @@
+#Reverse Words in A Given String
+#Problem Link: https://practice.geeksforgeeks.org/problems/reverse-words-in-a-given-string5459/1
+
+
+class Solution:
+    
+    #Function to reverse words in a given string.
+    def reverseWords(self,S):
+        array=S.split(".")
+        total=len(array)
+        S=""
+        for i in range(total-1,0,-1):
+           S+=array[i]
+           S+="."
+        S+=array[0]
+        return S
