8d23fc8728b53ed139451a6b44c15aa587eb82ab
ANUJJAINWIN <115687691+ANUJJAINWIN@users.noreply.github.com>
2022-10-13 12:10:47 +0530

    Create count-leaves-in-binary-tree.cpp

---
+++ Solutions/count-leaves-in-binary-tree.cpp
@@ -0,0 +1,12 @@
+// https://practice.geeksforgeeks.org/problems/count-leaves-in-binary-tree/1
+int countLeaves(Node* root)
+{
+  // Your code here
+  if(root==NULL)
+  return 0;
+  if(!root->left&&!root->right)
+  return 1;
+  
+  return countLeaves(root->left)+countLeaves(root->right);
+  
+}
