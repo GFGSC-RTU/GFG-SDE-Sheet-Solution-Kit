7c5c60404f29703eba5bb33411d16edd12da3a3e
Princi Mittal <87926701+princi2002@users.noreply.github.com>
2022-10-18 21:42:20 +0530

    Create Set kth bit.cpp

---
+++ Solutions/Set kth bit.cpp
@@ -0,0 +1,21 @@
+// C++ implementation to set the kth bit
+// of the given number
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+// function to set the kth bit
+int setKthBit(int n, int k)
+{
+    // kth bit of n is being set by this operation
+    return ((1 << k) | n);
+}
+ 
+// Driver program to test above
+int main()
+{
+    int n = 10, k = 2;
+    cout << "Kth bit set number = "
+         << setKthBit(n, k);
+    return 0;
+}
