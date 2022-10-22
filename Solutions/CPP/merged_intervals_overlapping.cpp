add9c91f3d87daadb4be0530084347f82e23cb85
dawnCoder1518 <116420641+dawnCoder1518@users.noreply.github.com>
2022-10-22 11:13:30 +0530

    created merged_intervals_overlapping.cpp

---
+++ Solutions/CPP/merged_intervals_overlapping.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        vector<vector<int>> mergedIntervals; 
+        if(intervals.size()==0)
+        {
+            return mergedIntervals;
+        }
+        else
+        {
+            sort(intervals.begin(),intervals.end());
+            vector<int> tempInterval = intervals[0];
+            for(auto it: intervals)
+            {
+                if(it[0]<=tempInterval[1])
+                {
+                    tempInterval[1] = max(tempInterval[1],it[1]);
+                }
+                else
+                {
+                    mergedIntervals.push_back(tempInterval);
+                    tempInterval = it;
+                }
+            }
+            mergedIntervals.push_back(tempInterval);
+            return mergedIntervals;
+        }
+    }
+};
