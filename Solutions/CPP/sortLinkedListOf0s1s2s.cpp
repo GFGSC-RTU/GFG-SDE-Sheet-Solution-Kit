6bc87d0228f4216c2167207d010457a5750a42c0
Anubhav404 <anubhavsoni.cse24@jecrc.ac.in>
2022-10-30 23:10:12 +0530

    cpp solution added in the cpp folder

--- Solutions/CPP/sortLinkedListOf0s1s2s.cpp
+++ Solutions/CPP/sortLinkedListOf0s1s2s.cpp
@@ -1,56 +1,73 @@
-Node* insertAtTail(Node* &tail , Node* curr){
-    tail ->next = curr;
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+Node *insertAtTail(Node *&tail, Node *curr)
+{
+    tail->next = curr;
     tail = curr;
 }
 
-
-Node* sortList(Node *head)
+Node *sortList(Node *head)
 {
     // Write your code here.
     Node *zeroHead = new Node(-1);
     Node *zeroTail = zeroHead;
-    
+
     Node *oneHead = new Node(-1);
     Node *oneTail = oneHead;
-    
+
     Node *twoHead = new Node(-1);
     Node *twoTail = twoHead;
-    
+
     Node *curr = head;
-    while(curr != NULL){
+    while (curr != NULL)
+    {
         int val = curr->data;
-        if(val == 0 ){
-            insertAtTail(zeroTail , curr);
+        if (val == 0)
+        {
+            insertAtTail(zeroTail, curr);
         }
-        else if(val == 1 ){
-            insertAtTail(oneTail 
- curr);
+        else if (val == 1)
+        {
+            insertAtTail(oneTail
+                             curr);
         }
-        else if(val == 2 ){
-            insertAtTail(twoTail 
- curr);
+        else if (val == 2)
+        {
+            insertAtTail(twoTail
+                             curr);
         }
-        curr = curr -> next;
+        curr = curr->next;
     }
-    
-    
-    // now merge the 3 linked list which we have created 
-    if(oneHead -> next != NULL){
-        zeroTail -> next = oneHead->next;
+
+    // now merge the 3 linked list which we have created
+    if (oneHead->next != NULL)
+    {
+        zeroTail->next = oneHead->next;
     }
-    else {
-        zeroTail -> next = twoHead->next;
+    else
+    {
+        zeroTail->next = twoHead->next;
     }
     oneTail->next = twoHead->next;
     twoTail->next = NULL;
-    
-    head = zeroHead -> next;
-    
+
+    head = zeroHead->next;
+
     delete zeroHead;
     delete oneHead;
     delete twoHead;
 
-    
     return head;
-
 }
