2b29dd432643d4f5022ab977a2cdc19c2d09a80c
Anubhav404 <anubhavsoni.cse24@jecrc.ac.in>
2022-10-30 23:08:06 +0530

    cpp solution added in the cpp folder

---
+++ Solutions/CPP/sortLinkedListOf0s1s2s.cpp
@@ -0,0 +1,56 @@
+Node* insertAtTail(Node* &tail , Node* curr){
+    tail ->next = curr;
+    tail = curr;
+}
+
+
+Node* sortList(Node *head)
+{
+    // Write your code here.
+    Node *zeroHead = new Node(-1);
+    Node *zeroTail = zeroHead;
+    
+    Node *oneHead = new Node(-1);
+    Node *oneTail = oneHead;
+    
+    Node *twoHead = new Node(-1);
+    Node *twoTail = twoHead;
+    
+    Node *curr = head;
+    while(curr != NULL){
+        int val = curr->data;
+        if(val == 0 ){
+            insertAtTail(zeroTail , curr);
+        }
+        else if(val == 1 ){
+            insertAtTail(oneTail 
+ curr);
+        }
+        else if(val == 2 ){
+            insertAtTail(twoTail 
+ curr);
+        }
+        curr = curr -> next;
+    }
+    
+    
+    // now merge the 3 linked list which we have created 
+    if(oneHead -> next != NULL){
+        zeroTail -> next = oneHead->next;
+    }
+    else {
+        zeroTail -> next = twoHead->next;
+    }
+    oneTail->next = twoHead->next;
+    twoTail->next = NULL;
+    
+    head = zeroHead -> next;
+    
+    delete zeroHead;
+    delete oneHead;
+    delete twoHead;
+
+    
+    return head;
+
+}
